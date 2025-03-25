8e9e1af4e3ba5135ca9afd079613294ed63b3d35
HH <jack.beanstalk.2022@gmail.com>
2025-03-24 23:42:55 -0700

    Add animation to the example

--- MySkiaComponent.tsx
+++ MySkiaComponent.tsx
@@ -1,13 +1,27 @@
 import {
+  useEffect,
+  useState,
+} from "react";
+import {
   Canvas,
   Circle,
 } from "@shopify/react-native-skia";
+import {
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 const MySkiaComponent = () => {
+  const radius = useSharedValue(128);
+  useEffect(() => {
+    radius.value = withTiming(50, { duration: 2000 });
+  }, []);
   return (
-    <Canvas style={{ flex: 1 }}>
-      <Circle r={128} cx={128} cy={128} color="red" />
-    </Canvas>
+    <div>
+      <Canvas style={{ flex: 1 }}>
+        <Circle r={radius} cx={128} cy={128} color="red" />
+      </Canvas>
+    </div>
   );
 }
 export default MySkiaComponent;
